9bad2a0dfc3fc37264b6e447904597341bce8ef4
DelcioSbeghen <spam@eavnet.com.br>
2025-03-28 23:53:39 -0300

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,8 +101,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.srpsis.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "67KBfLeBYO1Vh3ApGV4HjpgbzXwYRd+i2de1M0cRpL8=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
